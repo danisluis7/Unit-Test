459a06832910fe660975467764dafc5d1cabd02c
danisluis6 <vuongluis@hotmail.com>
2017-02-27 14:05:48 +0700

    level 2

---
+++ ApplyUnitTest/src/JavaFactoryPattern/Pass.java
@@ -0,0 +1,11 @@
+package JavaFactoryPattern;
+
+public class Pass implements Widget{
+
+	@Override
+	public boolean validate(String temp) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
